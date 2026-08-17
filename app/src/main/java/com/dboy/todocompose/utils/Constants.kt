f83d305fc2ad62990a0af41fcd5b49c97456e203
Daniel Andrioli <andrioli.daniel@yahoo.com.br>
2026-08-17 18:24:08 -0300

    Limite de caracteres no nome da task aumentado.

--- app/src/main/java/com/dboy/todocompose/utils/Constants.kt
+++ app/src/main/java/com/dboy/todocompose/utils/Constants.kt
@@ -3,7 +3,7 @@ package com.dboy.todocompose.utils
 object Constants {
     const val DATABASE_TODO_TABLE = "todo_table"
     const val DATABASE_NAME = "todo_db"
-    const val TASK_TITLE_MAX_CHARS = 30
+    const val TASK_TITLE_MAX_CHARS = 50
     const val PREFERENCE_NAME = "todo_preferences"
     const val PREFERENCE_KEY = "sort_state"
     const val SPLASH_SCREEN_DELAY = 1700L
